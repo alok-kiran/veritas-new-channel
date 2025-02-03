246ed19390534518e335506df721f54f21533671
alok-kiran <alokkiran777@gmail.com>
2025-02-04 02:06:49 +0400

    Fetch news articles in descending order by creation date; enhance featured news component to display tags

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -2,6 +2,10 @@ import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
 
 export async function GET() {
-  const all_news = await prisma.news.findMany({});
+  const all_news = await prisma.news.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
   return NextResponse.json({ news: all_news });
 }
